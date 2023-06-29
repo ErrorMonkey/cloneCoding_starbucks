6e3ceb91ee067f9f1022a51efc0dde649ee5cf04
ErrorMonkey <puts.hello.world.return0@gmail.com>
2023-06-29 19:34:58 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,27 +1,9 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener(('click'), function() {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
 // _.throttle(함수, 딜레이 시키고 싶은 시간)
 window.addEventListener('scroll', _.throttle(function () {
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
   if (window.scrollY > 500) {
     // gsap.to(요소, 지속시간, 옵션);
     gsap.to(badgeEl, .3, {
@@ -144,7 +126,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
